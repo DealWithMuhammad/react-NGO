ca9cff150da08ea504cc40e4fd28df29de9efb87
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-03-03 20:31:56 +0800

    removed unnecessary files

--- components/HeroVideo/index.jsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import React from "react";
-
-const Index = () => {
-  return <img class="w-screen h-screen" src="/hero.jpg" alt="Hero Image" />;
-};
-
-export default Index;
