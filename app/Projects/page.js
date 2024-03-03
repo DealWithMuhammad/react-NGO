f727f6a7a12704163095e8467fea27ae9e3ad4e5
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-03-03 20:15:00 +0800

    add features to project page

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -1,10 +1,11 @@
 import BlogsListing from "components/BlogsListing";
+import Feature from "components/Features";
 
 export default function page() {
   return (
     <div>
       {/* <BlogsListing all={true} category={"PROJECTS"} /> */}
-      nj
+      <Feature />
     </div>
   );
 }
