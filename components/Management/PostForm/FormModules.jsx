75cb8c6c6a4ec230c8edcb9f1dfa076d11a6ca63
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-03-06 18:35:09 +0800

    testing

--- components/Management/PostForm/FormModules.jsx
+++ components/Management/PostForm/FormModules.jsx
@@ -1,45 +1,43 @@
-
 const modules = {
-    toolbar: [
-        [{ header: '1' }, { header: '2' }, { font: [] }],
-        [{ size: [] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [
-            { list: 'ordered' },
-            { list: 'bullet' },
-            { indent: '-1' },
-            { indent: '+1' },
-        ],
-        ['link', 'video'],
-        ['clean'],
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
     ],
-    clipboard: {
-        // toggle to add extra line breaks when pasting HTML:
-        matchVisual: false,
-    },
-}
+    ["link", "video"],
+    ["clean"],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
 /*
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
  */
 const formats = [
-    'header',
-    'font',
-    'size',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    // 'image',
-    'video',
-]
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  // 'image',
+  "video",
+];
 
-const categoryOptions = ["PROJECTS", "BLOG"];
+const categoryOptions = ["BLOG"];
 
-
-export { modules, formats, categoryOptions }
+export { modules, formats, categoryOptions };
