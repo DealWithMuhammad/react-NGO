836c7d9661d8da38d96737a4d9d897bbf19b1fc3
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-03-07 18:00:09 +0800

    fixed error

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -1,40 +1,5 @@
-"use client";
-import featuresData from "./featuresData";
-import SingleFeature from "./SingleFeature";
-import SectionHeader from "../Common/SectionHeader";
-
 const Feature = () => {
-  return (
-    <>
-      {/* <!-- ===== Features Start ===== --> */}
-      <section id="features" className="py-20 lg:py-25 bg-[#ededed] xl:py-30">
-        <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
-          {/* <!-- Section Title Start --> */}
-          <SectionHeader
-            headerInfo={{
-              title: "",
-              subtitle: "Projects of ReAct",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-            convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam
-            ante in maximus.`,
-            }}
-          />
-          {/* <!-- Section Title End --> */}
-
-          <div className="mt-12.5 flex flex-wrap justify-center  gap-7.5  xl:mt-20 xl:gap-12.5">
-            {/* <!-- Features item Start --> */}
-
-            {featuresData.map((feature, key) => (
-              <SingleFeature feature={feature} key={key} />
-            ))}
-            {/* <!-- Features item End --> */}
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- ===== Features End ===== --> */}
-    </>
-  );
+  return <>hi</>;
 };
 
 export default Feature;
