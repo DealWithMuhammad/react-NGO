6fd98900e34e6d091a92bbeb09a594330a30465f
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-04-18 14:38:10 +0800

    added link in card so can open link in new tab

--- components/Services/SingleFeature.tsx
+++ components/Services/SingleFeature.tsx
@@ -31,28 +31,30 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
         transition={{ duration: 1, delay: 0.1 }}
         viewport={{ once: true }}
       >
-        <Card
-          className="w-full max-w-96 mx-auto"
-          shadow="lg"
-          isPressable
-          onClick={() => navigate.push(`services/${feature.id}`)}
-        >
-          <CardBody className="overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={feature.title}
-              className="w-full object-cover h-72 transition-transform transform hover:scale-x-105"
-              src={feature.imageUrl}
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-center mt-2 ">
-            <b className="transition-colors text-center duration-300 hover:text-[#f8cf2c]">
-              {subtitle}
-            </b>
-          </CardFooter>
-        </Card>
+        <Link href={`services/${feature.id}`}>
+          <Card
+            className="w-full max-w-96 mx-auto"
+            shadow="lg"
+            isPressable
+            onClick={() => navigate.push(`services/${feature.id}`)}
+          >
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={feature.title}
+                className="w-full object-cover h-72 transition-transform transform hover:scale-x-105"
+                src={feature.imageUrl}
+              />
+            </CardBody>
+            <CardFooter className="text-small justify-center mt-2 ">
+              <b className="transition-colors text-center duration-300 hover:text-[#f8cf2c]">
+                {subtitle}
+              </b>
+            </CardFooter>
+          </Card>
+        </Link>
       </motion.div>
     </>
   );
